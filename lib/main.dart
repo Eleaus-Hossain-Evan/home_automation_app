50393feb55219849424c59a152dd2302330258bb
Eleaus-Hossain-Evan <eleaus16-364@diu.edu.bd>
2023-11-06 18:53:17 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:home_automation_app/routes/app.routes.dart';
+import 'package:home_automation_app/styles/themes.dart';
+
+void main() {
+  runApp(
+    const ProviderScope(child: HomeAutomationApp())
+  );
+}
+
+class HomeAutomationApp extends ConsumerWidget {
+  const HomeAutomationApp({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+
+    return MaterialApp.router(
+      title: 'Home Automation',
+      theme: HomeAutomationTheme.light,
+      darkTheme: HomeAutomationTheme.dark,
+      themeMode: ThemeMode.system,
+      debugShowCheckedModeBanner: false,
+      routeInformationProvider: AppRoutes.router.routeInformationProvider,
+      routeInformationParser: AppRoutes.router.routeInformationParser,
+      routerDelegate: AppRoutes.router.routerDelegate,
+    );
+  }
+}
