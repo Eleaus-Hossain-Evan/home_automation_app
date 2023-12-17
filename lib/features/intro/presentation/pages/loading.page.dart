b755727b8a59e41184843fd2cd8faec0b2b3fdc9
Eleaus-Hossain-Evan <eleaus16-364@diu.edu.bd>
2023-12-17 19:11:06 +0600

    add rive animation to LoadingPage. ep4 done

--- lib/features/intro/presentation/pages/loading.page.dart
+++ lib/features/intro/presentation/pages/loading.page.dart
@@ -1,14 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:home_automation_app/styles/styles.dart';
+import 'package:rive/rive.dart ' as rive;
 
-class LoadingPage extends StatelessWidget {
+class LoadingPage extends StatefulWidget {
   static const String route = '/loading';
   const LoadingPage({super.key});
 
   @override
+  State<LoadingPage> createState() => _LoadingPageState();
+}
+
+class _LoadingPageState extends State<LoadingPage> {
+  late rive.StateMachineController smController;
+  late rive.RiveAnimation animation;
+  late Map<Brightness, rive.SMIBool> states = {};
+  bool isInitialized = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    animation = rive.RiveAnimation.asset(
+      'assets/anims/flicky.riv',
+      artboard: 'flickylogo',
+      fit: BoxFit.fitHeight,
+      onInit: onRiveInit,
+    );
+  }
+
+  void onRiveInit(rive.Artboard artboard) {
+    smController =
+        rive.StateMachineController.fromArtboard(artboard, 'flickylogo')!;
+
+    artboard.addController(smController);
+    // states[Theme.of(context).brightness] =
+    //     smController.findInput<bool>('loaded') as rive.SMIBool;
+
+    for (var theme in Brightness.values) {
+      states[theme] = smController.findInput<bool>(theme.name) as rive.SMIBool;
+      states[theme]!.value = false;
+    }
+
+    setState(() {
+      isInitialized = true;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    final theme = MediaQuery.platformBrightnessOf(context);
+    if (isInitialized) {
+      for (var valueThemes in Brightness.values) {
+        states[valueThemes]!.value = theme == valueThemes;
+      }
+    }
+    return Scaffold(
       body: Center(
-        child: Text("Loading Page"),
+        child: SizedBox(
+          width: HomeAutomationStyles.loadingIconSize,
+          height: HomeAutomationStyles.loadingIconSize,
+          child: animation,
+        ),
       ),
     );
   }
